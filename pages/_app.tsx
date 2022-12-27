10bb7d6e33474c2814148a11e97cfd31a92b0c3e
pmb087 <pmb087@gmail.com>
2022-12-27 17:17:04 +0900

    feat: recoil, swr, axios 셋팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,26 @@
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+import { SWRConfig } from 'swr';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../styles/GlobalStyle';
 import theme from '../styles/theme';
+import client from '../utils/CustomAxios';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <RecoilRoot>
+      <SWRConfig
+        value={{
+          refreshInterval: 5000,
+          fetcher: (url: string) => client.get(url).then((res) => res.data)
+        }}
+      >
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SWRConfig>
+    </RecoilRoot>
   );
 }
 
