10045064898e3e6e75221d1b5b5f9429067b62ff
pmb087 <pmb087@gmail.com>
2023-01-06 16:40:32 +0900

    feat: Map 페이지

---
+++ components/Info.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+interface Props {
+  title: string;
+  content: string | string[];
+}
+
+function Info({ title, content }: Props) {
+  return (
+    <InfoWrap>
+      <InfoTitle>{title}</InfoTitle>
+      {!Array.isArray(content) ? (
+        <InfoContent>{content}</InfoContent>
+      ) : (
+        content.map((item: string) => (
+          <InfoContent key={item}>{item}</InfoContent>
+        ))
+      )}
+    </InfoWrap>
+  );
+}
+
+export default Info;
+
+const InfoWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 40px;
+`;
+
+const InfoTitle = styled.h1`
+  font-size: 34px;
+  font-weight: bold;
+  color: #505050;
+  margin-bottom: 15px;
+  letter-spacing: 1.2px;
+`;
+
+const InfoContent = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  color: #808080;
+  margin-bottom: 5px;
+  letter-spacing: 1px;
+`;
