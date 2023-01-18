f91c4935ce021be64a7301c793f36fcfe197374d
pmb087 <pmb087@gmail.com>
2023-01-18 20:23:25 +0900

    feat: 가게 추가 요청 관리 페이지 추가

---
+++ components/Management.tsx
@@ -0,0 +1,97 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import StoreService from '../utils/service/StoreService';
+import { RequestGetContent } from '../utils/types';
+import RequestPostLink from './RequestPostLink';
+
+function Management() {
+  const [requestData, setRequestData] = useState<RequestGetContent[]>();
+
+  const getRequestData = async () => {
+    const { data } = await StoreService.getRequest();
+    setRequestData(data);
+  };
+
+  useEffect(() => {
+    getRequestData();
+  }, []);
+
+  return (
+    <ManagementContainer>
+      <ManagementTitle>문의 관리</ManagementTitle>
+      <ManagementPostContainer>
+        <RequestPostLinkContainer header={true}>
+          <RequestPostNumberHeader>접수 번호</RequestPostNumberHeader>
+          <RequestPostNameHeader>요청 가게</RequestPostNameHeader>
+        </RequestPostLinkContainer>
+        {requestData &&
+          requestData.map((el: RequestGetContent, index: number) => (
+            <RequestPostLink
+              key={el.id}
+              currentRequest={el}
+              setRequest={setRequestData}
+              allRequest={requestData}
+              index={index}
+            />
+          ))}
+      </ManagementPostContainer>
+    </ManagementContainer>
+  );
+}
+
+export default Management;
+
+type PostHeader = {
+  header: boolean;
+};
+
+const ManagementContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: 20px;
+`;
+
+const ManagementTitle = styled.div`
+  margin: 20px;
+  padding: 20px 20px 20px 50px;
+  border-bottom: 2px solid #808080;
+  font-size: 30px;
+  font-weight: bolder;
+  color: #505050;
+`;
+
+const ManagementPostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  width: 100%;
+`;
+
+const RequestPostLinkContainer = styled.div<PostHeader>`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  border-bottom: 1px solid #c0c0c0;
+  font-size: 20px;
+  font-weight: 600;
+  color: #444444;
+  :hover {
+    background-color: ${({ header }) => (header ? 'none' : '#ff904d1a')};
+  }
+`;
+
+const RequestPostNumberHeader = styled.p`
+  width: 120px;
+  text-align: center;
+  :hover {
+    color: #ff904d;
+    cursor: pointer;
+  }
+`;
+
+const RequestPostNameHeader = styled.p`
+  width: calc(100% - 120px - 120px - 60px);
+  text-align: center;
+`;
