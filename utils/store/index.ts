148e7ae7cbe3b9b7e36bfda5d71b6c0644d8ef77
pmb087 <pmb087@gmail.com>
2023-01-26 13:29:28 +0900

    delete: 사용하지 않은 라이브러리 제거

--- utils/store/index.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'recoil';
-import { CurrentUser } from '../types';
-
-export const userInfoState = atom<CurrentUser>({
-  key: 'userInfo',
-  default: {
-    email: ''
-  }
-});
